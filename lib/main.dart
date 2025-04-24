17c08437f22512ac98bbec0cdc1a3ea1596b7952
omerbanaemh <omerbanaemh@gmail.com>
2025-04-25 01:21:45 +0300

    Add flutter_screenutil dependency and set-up config in main file for resposive design

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,50 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(
-    GetMaterialApp(
-      title: "Onyx Delivery App",
-      debugShowCheckedModeBanner: false,
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
+    const MyApp(),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+        minTextAdapt: true,
+        splitScreenMode: true,
+        designSize: const Size(360, 690),
+        builder: (context, child) {
+          SystemChrome.setPreferredOrientations([
+            DeviceOrientation.portraitUp,
+            DeviceOrientation.portraitUp,
+          ]);
+          return GetMaterialApp(
+            title: "Onyx Delivery App",
+            debugShowCheckedModeBanner: false,
+            builder: (context, child) {
+              ScreenUtil.init(context);
+              return MediaQuery(
+                data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+                child: Scaffold(
+                  body: child ?? const SizedBox(),
+                ),
+              );
+            },
+            locale: const Locale("en"),
+            supportedLocales: const [
+              Locale('en', 'US'),
+              Locale('ar', ''),
+            ],
+            initialRoute: AppPages.INITIAL,
+            getPages: AppPages.routes,
+          );
+        });
+  }
 }
